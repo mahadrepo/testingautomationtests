028575e89f3fd1fa7facfc392d1ddf06b6d5efca
mahad nadeem <mahad.nadeem@dotfoods.com>
2024-10-14 14:57:48 +0500

    Initial commit

---
+++ SimpleFunctionApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleFunctionApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        } 
+        //testttttt
+
+        public static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
